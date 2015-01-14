920f1da94ec68e526f9579b09d2610110cab4a80
Ryan Pfeffer <rdpfeffer@gmail.com>
2015-01-14 20:09:14 +0000

    Missing Comma

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license=open('LICENSE').read(),
     author='Michael Palumbo',
     author_email='michael.palumbo87@gmail.com',
-    description='HeapAnalytics Python library for server-side integration.'
+    description='HeapAnalytics Python library for server-side integration.',
     long_description=open('README.md').read(),
     keywords='heap analytics python',
 )
