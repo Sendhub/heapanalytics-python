337a6aae17d0ac11dd13d802264af90546c53256
Ryan Pfeffer <rdpfeffer@gmail.com>
2015-01-14 20:05:53 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='heapanalytics-python',
+    version='0.1',
+    packages=[''],
+    license=open('LICENSE').read(),
+    author='Michael Palumbo',
+    author_email='michael.palumbo87@gmail.com',
+    description='HeapAnalytics Python library for server-side integration.'
+    long_description=open('README.md').read(),
+    keywords='heap analytics python',
+)
